5c4258fa4262c086f315578c2febd2a3ce701f3d
Paul Salamone <93609282+paulsalamone@users.noreply.github.com>
2021-11-29 13:35:24 +0100

    Update jokes.js

--- jokes.js
+++ jokes.js
@@ -1,11 +1,11 @@
 const jokes = [
-  '"A lot of people are afraid of heights. Not me, I’m afraid of widths." --Steve Wright',
-  '"All those who believe in psychokinesis raise my hand." --Steve Wright',
-  '"Ambition is a poor excuse for not having enough sense to be lazy." --Steve Wright',
-  '"Bills travel through the mail at twice the speed of checks." --Steve Wright',
-  '"Borrow money from pessimists-they don’t expect it back." --Steve Wright',
-  '"Change is inevitable….except from vending machines." --Steve Wright',
-  '"Cross country skiing is great if you live in a small country." --Steve Wright',
+  '"A lot of people are afraid of heights. Not me, I’m afraid of widths." --Steven Wright',
+  '"All those who believe in psychokinesis raise my hand." --Steven Wright',
+  '"Ambition is a poor excuse for not having enough sense to be lazy." --Steven Wright',
+  '"Bills travel through the mail at twice the speed of checks." --Steven Wright',
+  '"Borrow money from pessimists-they don’t expect it back." --Steven Wright',
+  '"Change is inevitable….except from vending machines." --Steven Wright',
+  '"Cross country skiing is great if you live in a small country." --Steven Wright',
 ];
 
 const jokeGen = (array) => {
